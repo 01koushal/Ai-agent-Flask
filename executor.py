ce81785db3e42628d82d5195b152656f2ad986b7
Koushal Pala <koushalpala@gmail.com>
2026-01-10 16:05:45 +0530

    Enhance execute function with new operations and filters
    Updated the find_one and find_many operations to exclude 'embedding' and 'text_for_embedding' fields from the results. Added support for group_count operation with aggregation pipeline.

--- executor.py
+++ executor.py
@@ -9,10 +9,36 @@ def execute(plan: dict):
         return collection.count_documents(filt)
 
     if op == "find_one":
-        return collection.find_one(filt, {"_id": 0})
+        return collection.find_one(
+        filt,
+        {
+            "_id": 0,
+            "embedding": 0,
+            "text_for_embedding": 0
+        }
+    )
+
 
     if op == "find_many":
         return list(
-            collection.find(filt, {"_id": 0})
-            .limit(plan.get("limit", 10))
-        )
+        collection.find(
+            filt,
+            {
+                "_id": 0,
+                "embedding": 0,
+                "text_for_embedding": 0
+            }
+        ).limit(plan.get("limit", 10))
+    )
+
+    if op == "group_count":
+        field = plan["group_by"]
+
+    pipeline = [
+        {"$group": {"_id": f"${field}", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}
+    ]
+
+    result = list(collection.aggregate(pipeline))
+    return {r["_id"]: r["count"] for r in result}
+
